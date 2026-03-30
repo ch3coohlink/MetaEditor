62b7c275da3efef9cf8ae0f713120398e4df3326
ch3coohlink <chiiicoohlink@gmail.com>
2026-03-30 17:21:32 +0800

    refactor(scripts): simplify test scripts and remove unused files

--- scripts/test-native.ps1
+++ scripts/test-native.ps1
@@ -1,9 +1,1 @@
-$ErrorActionPreference = 'Stop'
-
 & "$PSScriptRoot\build-native.ps1" -Package service -Test -TestPackage service -TestFilter "native:*"
-if ($LASTEXITCODE -ne 0) {
-  exit $LASTEXITCODE
-}
-
-& "$PSScriptRoot\test-service-lifecycle.ps1"
-exit $LASTEXITCODE
